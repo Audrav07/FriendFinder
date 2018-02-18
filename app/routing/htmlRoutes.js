e425a052c585da27a786ff79d0758b8a325bf51e
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-02-17 18:37:05 -0700

    editing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,53 +1,12 @@
-// //Dependencies
-// var path = require('path');
-
-// //ROUTING
-
-// module.exports = function(app){
-//   //default GET route that leads to home.html - displays home page
-//   app.get('/survey', function (req, res) {
-//     res.sendFile(path.join(__dirname + '/../public/survey.html'));
-//   });
-
-//   //a USE route to home page
-//   app.use(function (req, res) {
-//     res.sendFile(path.join(__dirname + '/../public/home.html'));
-//   });
-// };
-
-
-
-
-
-
-
-
-const express = require('express');	
+const express = require('express');
 const path = require("path");
-	
-
-
-	module.exports = function(app){
+module.exports = function(app) {
   //default GET route that leads to home.html - displays home page
-  app.get('/survey', function (req, res) {
+  app.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
-
   //a USE route to home page
-  app.use(function (req, res) {
+  app.use(function(req, res) {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 };
-
-// 	module.exports = app => {
-	
-// 	// default root to home.html
-// 	app.get("/", function(req, res) {
-// 	res.sendFile(path.join(__dirname, "/../public/home.html"))
-// 	})
-// 	app.get("/survey", function(req, res) {
-// 	res.sendFile(path.join(__dirname, "/../public/survey.html"))
-// 	})
-	
-// 	app.use((req, res) => { res.send('404') });
-// };
